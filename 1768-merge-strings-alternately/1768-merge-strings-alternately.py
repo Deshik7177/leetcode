6e109a4592eeb41e1746e82286069633a0935610
DESHIK <145757793+Deshik7177@users.noreply.github.com>
2026-07-08 03:47:08 +0530

    Time: 43 ms (71.84%), Space: 19.3 MB (16.94%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        arr=""
+        # if len(word1) == len(word2):
+        #     for i in range(len(word1)):
+        #         arr += word1[i]
+        #         arr += word2[i]
+        n=min(len(word1),len(word2))
+        for i in range(n):
+            arr += word1[i]
+            arr += word2[i]
+        if n==len(word1):
+            arr+=word2[n:]
+        elif n==len(word2):
+            arr+=word1[n:]
+        return arr
