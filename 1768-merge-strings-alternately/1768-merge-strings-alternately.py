0a09ad7d06b3710358ca69cd09d69490d67d2204
DESHIK <145757793+Deshik7177@users.noreply.github.com>
2026-07-08 11:01:10 +0530

    Time: 55 ms (6.24%), Space: 19.2 MB (87.61%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,12 +1,12 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        arr=""
-        n=min(len(word1),len(word2))
+        arr = ""
+        n = min(len(word1),len(word2))
         for i in range(n):
             arr += word1[i]
             arr += word2[i]
         if n==len(word1):
-            arr+=word2[n:]
+            arr += word2[n:]
         elif n==len(word2):
-            arr+=word1[n:]
+            arr += word1[n:]
         return arr
