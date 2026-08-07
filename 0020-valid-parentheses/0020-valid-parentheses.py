319636502ce1bff530fd12e4391d2db69b0fc313
DESHIK <145757793+Deshik7177@users.noreply.github.com>
2026-08-07 10:46:58 +0530

    Time: 0 ms (100%), Space: 19.2 MB (91.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,19 +4,20 @@ class Solution:
         for ch in s:
             if ch in "([{":
                 stack.append(ch)
+
             else:
                 if not stack:
                     return False
 
                 if ch == ")" and stack[-1] != "(":
                     return False
-
+                
                 if ch == "]" and stack[-1] != "[":
                     return False
-
+                
                 if ch == "}" and stack[-1] != "{":
                     return False
 
                 stack.pop()
-                
+
         return len(stack) == 0
